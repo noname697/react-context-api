4e2187876cd32522a0d14b733a054e8db4ec793c
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-07-02 17:06:03 -0300

    Passando o Provider para o App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { CarrinhoProvider } from "./context/CarrinhoContext";
 import Home from "./pages/Home";
 import Carrinho from "./pages/Carrinho";
 import PaginaErro from "./pages/PaginaErro";
@@ -9,11 +9,13 @@ import "./App.css";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/carrinho" element={<Carrinho />} />
-        <Route path="*" element={<PaginaErro />} />
-      </Routes>
+      <CarrinhoProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/carrinho" element={<Carrinho />} />
+          <Route path="*" element={<PaginaErro />} />
+        </Routes>
+      </CarrinhoProvider>
     </BrowserRouter>
   );
 }
