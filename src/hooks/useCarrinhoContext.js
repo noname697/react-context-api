4e61f6fda4c9babc46102ad8a3c4497d712bbcc0
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-07-06 14:14:49 -0300

    Funcao de remover produto com o hook customizado

--- src/hooks/useCarrinhoContext.js
+++ src/hooks/useCarrinhoContext.js
@@ -43,10 +43,18 @@ export const useCarrinhoContext = () => {
     setCarrinho([...carrinhoAtualizado]);
   }
 
+  function removerProdutoCarrinho(id) {
+    const produto = carrinho.filter((itemDoCarrinho) => {
+      return itemDoCarrinho.id !== id;
+    });
+    setCarrinho(produto);
+  }
+
   return {
     carrinho,
     setCarrinho,
     adicionarProduto,
     removerProduto,
+    removerProdutoCarrinho,
   };
 };
